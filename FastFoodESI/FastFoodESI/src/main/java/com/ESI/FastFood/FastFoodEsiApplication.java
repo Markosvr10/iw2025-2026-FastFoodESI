ade8a75ecef53c85ee868976bea49cef610950be
LuisTN19 <127325207+LuisTN19@users.noreply.github.com>
2025-10-24 17:29:27 +0200

    Add files via upload
    inicio

---
+++ FastFoodESI/FastFoodESI/src/main/java/com/ESI/FastFood/FastFoodEsiApplication.java
@@ -0,0 +1,13 @@
+package com.ESI.FastFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodEsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodEsiApplication.class, args);
+	}
+
+}
---
+++ FastFoodESI/FastFoodESI/src/test/java/com/ESI/FastFood/FastFoodEsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ESI.FastFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodEsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

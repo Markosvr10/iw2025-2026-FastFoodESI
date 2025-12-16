3e9142e60d35b9c40c4a9e9fcd41b4d3b7c8cb8e
smormen13 <salvamoreno02@gmail.com>
2025-12-16 19:41:46 +0100

    Layout y Menú Principal

---
+++ FastFoodESI/FastFoodESI/src/main/java/com/ESI/FastFoodESI/ui/views/HomeView.java
@@ -0,0 +1,27 @@
+package com.ESI.FastFoodESI.ui.views;
+
+import com.ESI.FastFoodESI.ui.views.admin.NegociosView;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.PermitAll;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Route("") 
+@PermitAll 
+public class HomeView extends VerticalLayout implements BeforeEnterObserver {
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        
+        if (auth != null && auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_PROPRIETARIO"))) {
+            
+            event.forwardTo(NegociosView.class);
+            
+        } 
+    }
+}

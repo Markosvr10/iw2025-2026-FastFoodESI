0ee428063af6a624d6c88957eb6fdeb671627542
LuisTN19 <luistarnar19@gmail.com>
2025-12-24 21:12:20 +0100

    añadir método findByNombre para recuperar estados por texto

--- FastFoodESI/FastFoodESI/src/main/java/com/ESI/FastFoodESI/repository/EstadoPedidoRepository.java
+++ FastFoodESI/FastFoodESI/src/main/java/com/ESI/FastFoodESI/repository/EstadoPedidoRepository.java
@@ -4,8 +4,10 @@ import com.ESI.FastFoodESI.model.EstadoPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EstadoPedidoRepository extends JpaRepository<EstadoPedido, UUID> {
+    Optional<EstadoPedido> findByNombre(String nombre);
 }

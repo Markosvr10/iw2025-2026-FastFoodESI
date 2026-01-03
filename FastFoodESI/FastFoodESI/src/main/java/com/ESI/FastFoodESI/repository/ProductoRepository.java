d1b6d8524fab41927793830e23f66eac76103be1
smormen13 <salvamoreno02@gmail.com>
2026-01-03 19:01:12 +0100

    Update ProductoRepository.java

--- FastFoodESI/FastFoodESI/src/main/java/com/ESI/FastFoodESI/repository/ProductoRepository.java
+++ FastFoodESI/FastFoodESI/src/main/java/com/ESI/FastFoodESI/repository/ProductoRepository.java
@@ -5,7 +5,6 @@ import com.ESI.FastFoodESI.model.Negocio;
 import com.ESI.FastFoodESI.model.Producto;
 import com.ESI.FastFoodESI.model.Propietario;
 
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
